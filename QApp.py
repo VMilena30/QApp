e9e2546571bd19d814f1112b11a8124c2088182a
VMilena30 <vmilena3010@gmail.com>
2025-07-03 10:35:20 -0300

    Update QApp.py

--- QApp.py
+++ QApp.py
@@ -171,7 +171,7 @@ TEXTOS_OPT = {
         <div style="background-color: #f9f9f9; margin: 0; padding: 12px; border-radius: 5px; border: 1px solid #ddd; max-width: 850px; font-size: 14px; line-height: 1.4;">
             <h4 style="color: #333; font-size: 16px; margin: 8px 0;">Instruções para Upload</h4>
             <p style="margin: 4px 0;">O arquivo de entrada deve ser um arquivo de texto (.txt), onde cada linha representa uma instância, com o seguinte formato:</p>
-            <p style="background-color: #eee; padding: 8px; border-radius: 5px; font-size: 14px; margin: 4px 0;"><code>[s, nj_max, nj_min, ctj_of, Rjk_of, cjk_of, C_of]</code></p>
+            <p style="background-color: #eee; padding: 8px; border-radius: 5px; font-size: 10px; margin: 4px 0;"><code>[s, nj_max, nj_min, ctj_of, Rjk_of, cjk_of, C_of]</code></p>
             <p style="margin: 4px 0;">Certifique-se de que o arquivo siga exatamente este formato para que os dados sejam lidos corretamente.</p>
             <p style="margin: 4px 0;">Clique no botão abaixo para baixar um arquivo de teste já formatado.</p>
         </div>
