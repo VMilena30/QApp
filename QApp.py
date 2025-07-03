e0b00be2f9288367234d1ac4f4ae4312656954c0
VMilena30 <vmilena3010@gmail.com>
2025-07-03 10:00:14 -0300

    Update QApp.py

--- QApp.py
+++ QApp.py
@@ -251,15 +251,14 @@ TEXTOS_OPT = {
             "$$ x_i \\in \\{0, 1, 2, \\ldots, n_{\\text{max}}\\} $$\n\n"
             
              "##### Termos Utilizados\n\n"
-                "1. \( x_i \): número de componentes redundantes no subsistema \( i \)  \n"
-                "2. \( R_i \): confiabilidade de um componente do subsistema \( i \)  \n"
-                "3. \( R(x) \): confiabilidade global do sistema  \n"
-                "4. \( c_i \): custo de adicionar um componente no subsistema \( i \)  \n"
-                "5. \( C \): orçamento máximo  \n"
-                "6. \( n_{\text{min}}, n_{\text{max}} \): limites inferior e superior para redundâncias  \n"
-
-
-            "A confiabilidade total considera subsistemas em paralelo, que falham somente se todos os componentes falharem.\n\n"
+            """
+            1. $x_i$: número de componentes redundantes no subsistema $i$  
+            2. $R_i$: confiabilidade de um componente do subsistema $i$  
+            3. $R(x)$: confiabilidade global do sistema  
+            4. $c_i$: custo de adicionar um componente no subsistema $i$  
+            5. $C$: orçamento máximo  
+            6. $n_{\\mathrm{min}}, n_{\\mathrm{max}}$: limites inferior e superior para redundâncias  
+            """
         ),
     },
     "en": {
