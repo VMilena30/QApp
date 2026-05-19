108691cb56c4a450fd5b0496f994f900a53d8b61
arthurdantasbc <arthur.dbcosta@ufpe.br>
2026-05-19 14:10:01 -0300

    Update QApp.py

--- QApp.py
+++ QApp.py
@@ -4479,6 +4479,7 @@ def main():
             # ===== MATRIZ DE CONFUSÃO =====
             from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
             import matplotlib.pyplot as plt
+            import seaborn as sns
         
             cm = confusion_matrix(y_test, y_pred)
 
